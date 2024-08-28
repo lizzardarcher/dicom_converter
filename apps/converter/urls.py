f688133623c85c350d990edd8711b2782882b3f0
ansel <populator@live.com>
2024-08-28 15:20:07 +0500

    dev

--- apps/converter/urls.py
+++ apps/converter/urls.py
@@ -5,4 +5,5 @@ from apps.converter import views
 urlpatterns = [
     # path('', views.HomeView.as_view(), name='home'),
     path('upload_research', views.UploadResearchView.as_view(), name='upload research'),
+    path('my_research', views.UploadResearchView.as_view(), name='my research'),
 ]
