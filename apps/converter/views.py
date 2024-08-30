b3ae8c5eca57ad794aa77ee8d8135d4fbc990b6b
ansel <populator@live.com>
2024-08-30 17:21:33 +0500

    dev

--- apps/converter/views.py
+++ apps/converter/views.py
@@ -13,7 +13,6 @@ from apps.converter.models import Research
 class UploadResearchView(SuccessMessageMixin, CreateView):
     model = Research
     form_class = ResearchUploadForm
-    success_url = '/'
     template_name = 'pages/upload_research.html'
 
     def get_context_data(self, **kwargs):
@@ -24,10 +23,10 @@ class UploadResearchView(SuccessMessageMixin, CreateView):
         return context
 
     def get_success_url(self):
-        return '/upload_research'
+        return '/accounts/profile/'
 
     def get_success_message(self, cleaned_data):
-        return f"Архив успешно обработан {cleaned_data['raw_archive'].name}"
+        return f"Архив успешно обработан {cleaned_data['raw_archive'].name}. Данные томографа сконвертированы в формат DICOM"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
