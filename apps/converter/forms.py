7f11d092155d21519830d45439814bd2739166f7
ansel <populator@live.com>
2024-09-10 18:47:16 +0500

    dev

--- apps/converter/forms.py
+++ apps/converter/forms.py
@@ -6,7 +6,8 @@ from apps.converter.models import Research
 class ResearchUploadForm(forms.ModelForm):
     class Meta:
         model = Research
-        fields = ['raw_archive', 'is_anonymous']
+        fields = ['raw_archive', 'is_anonymous', 'is_one_file']
         widgets = {
-            'is_anonymous': forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'flexSwitchCheckDefault'}),
+            'is_anonymous': forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'flexSwitchCheckDefault1', 'disabled': 'disabled'}),
+            'is_one_file': forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'flexSwitchCheckDefault2', 'disabled': 'disabled'}),
         }
