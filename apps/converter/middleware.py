451f889473fb9bab6332b08665662d1e4a9ec4d4
ansel <populator@live.com>
2024-10-21 15:33:42 +0500

    dev

---
+++ apps/converter/middleware.py
@@ -0,0 +1,19 @@
+from django.utils.deprecation import MiddlewareMixin
+
+
+class SecurityMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response['X-Content-Type-Options'] = 'nosniff'
+        return response
+
+
+class ReferrerPolicyMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+        return response
+
+
+class CSPMiddleware(MiddlewareMixin):
+  def process_response(self, request, response):
+    response['Content-Security-Policy'] = "script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+    return response
