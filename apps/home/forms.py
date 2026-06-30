e344e9e795d8e9dd8f432c8a246d4ad6c8e4b02a
ansel <populator@live.com>
2026-06-30 22:23:47 +0500

    dev

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -1,9 +1,11 @@
 from django import forms
+from django.utils.translation import pgettext_lazy
 from django_recaptcha.fields import ReCaptchaField
 
 
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=100)
+    name = forms.CharField(label=pgettext_lazy('contacts', 'Имя'), max_length=100)
     email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Сообщение', widget=forms.Textarea)
+    message = forms.CharField(label=pgettext_lazy('contacts', 'Сообщение'), widget=forms.Textarea)
     confirm = ReCaptchaField()
